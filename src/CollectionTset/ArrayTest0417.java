864e11919803abaae998b2b0be65fd16345dd4b6
wangfl5 <banwanjs@126.com>
2018-04-17 14:29:06 +0800

    测试提交

--- src/CollectionTset/ArrayTest0417.java
+++ src/CollectionTset/ArrayTest0417.java
@@ -1,17 +1,20 @@
 package CollectionTset;
+
+import java.util.Random;
+
 /**
  *@author wangfl5
  *@date 2018-4-17
  *@description
  **/
 public class ArrayTest0417 {
-
 	/**
 	 * 2018-4-17
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Random a = new Random();
+		System.out.println(a.nextInt(12));
 
 	}
 
